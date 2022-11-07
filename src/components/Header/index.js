4ee0c3d35fe26e1f09cf9677551df25e8193f91d
aistegerd <gerdzeviciute.yy@gmail.com>
2022-11-07 23:27:48 +0200

    feat: create initial Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './styles.module';
+import Logo from '~/assets/svg/logo.svg';
+
+const cn = classNames.bind(styles);
+
+export default function Header() {
+  return (
+    <header className={cn('header')}>
+      <div className={cn('brandWrapper')}>
+        <Logo className={cn('image')} />
+        <h1 className={cn('brandName')}>Sourcery Academy</h1>
+      </div>
+
+      <div className={cn('linkWrapper')}>
+        <a href="#">About us</a>
+        <a href="#">Academies</a>
+        <a href="#">Media</a>
+        <a href="#">Register</a>
+        <a href="#">Questions</a>
+      </div>
+    </header>
+  );
+}
