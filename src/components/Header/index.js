ba621b2f1e6007b6a0a54718b519ba2bfded2eed
aistegerd <gerdzeviciute.yy@gmail.com>
2022-11-08 23:12:42 +0200

    feat: add dropdown icon and adjust white space

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './styles.module';
 import Logo from '~/assets/svg/logo.svg';
+import IconArrowDown from '~/assets/svg/icon-arrow-down.svg';
 
 const cn = classNames.bind(styles);
 
@@ -12,10 +13,12 @@ export default function Header() {
         <Logo className={cn('image')} />
         <h1 className={cn('brandName')}>Sourcery Academy</h1>
       </div>
-
       <div className={cn('linkWrapper')}>
         <a href="#">About us</a>
-        <a href="#">Academies</a>
+        <a href="#">
+          Academies
+          <IconArrowDown className={cn('icon')} />
+        </a>
         <a href="#">Media</a>
         <a href="#">Register</a>
         <a href="#">Questions</a>
