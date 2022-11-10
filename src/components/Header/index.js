5e8430f90228bea26959e85f821547698394a820
aistegerd <gerdzeviciute.yy@gmail.com>
2022-11-10 10:03:05 +0200

    feat: make brand clickable, use rem instead px

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,13 +7,18 @@ import IconArrowDown from '~/assets/svg/icon-arrow-down.svg';
 const cn = classNames.bind(styles);
 
 export default function Header() {
+  const goToHome = () => {
+    // TODO: Implement routing logic
+    return;
+  };
+
   return (
     <header className={cn('header')}>
-      <div className={cn('brandWrapper')}>
+      <div className={cn('brandWrapper')} onClick={goToHome}>
         <Logo className={cn('image')} />
         <h1 className={cn('brandName')}>Sourcery Academy</h1>
       </div>
-      <div className={cn('linkWrapper')}>
+      <nav className={cn('nav')}>
         <a href="#">About us</a>
         <a href="#">
           Academies
@@ -22,7 +27,7 @@ export default function Header() {
         <a href="#">Media</a>
         <a href="#">Register</a>
         <a href="#">Questions</a>
-      </div>
+      </nav>
     </header>
   );
 }
