33add94807079468a7a86faafa4b6312a3f85d75
merinav <81377750+merinav@users.noreply.github.com>
2022-11-12 14:49:54 +0200

    SR22S-15 import and render .svg files

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Footer.module.scss';
-import FacebookLogo from '~/assets/svg/facebookLogo';
-import TwitterLogo from '~/assets/svg/twitterLogo';
-import InstagramLogo from '../../assets/svg/instagramLogo';
+import TwitterLogo from '~/assets/svg/twitterLogo.svg';
+import FacebookLogo from '../../assets/svg/facebookLogo.svg';
+import Svg from '../../assets/svg/instagramLogo.svg';
 
 const cn = classNames.bind(styles);
 const today = new Date();
@@ -16,21 +16,21 @@ const Footer = () => {
         <div className={cn('icon-box-facebook')}>
           <a href="https://www.facebook.com/devbridge">
             <div className={cn('icon-eclipse-facebook')}>
-              <FacebookLogo alt="Link to Facebook" />
+              <FacebookLogo alt="Link to Facebook" className={cn('image')} />
             </div>
           </a>
         </div>
         <div className={cn('icon-box-twitter')}>
           <a href="https://twitter.com/devbridge">
             <div className={cn('icon-eclipse-twitter')}>
-              <TwitterLogo alt="Link to Twitter" />
+              <TwitterLogo alt="Link to Twitter" className={cn('image')} />
             </div>
           </a>
         </div>
         <div className={cn('icon-box-instagram')}>
           <a href="https://www.instagram.com/devbridge/?hl=en">
             <div className={cn('icon-eclipse-instagram')}>
-              <InstagramLogo alt="Link to Instagram" />
+              <Svg alt="Link to Instagram" className={cn('image')} />
             </div>
           </a>
         </div>
