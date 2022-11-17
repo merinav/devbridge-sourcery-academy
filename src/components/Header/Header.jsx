b129d442dc0231d79aeb021a4b040681656c6e1a
merinav <81377750+merinav@users.noreply.github.com>
2022-11-17 10:13:41 +0200

    SR22S-24 Tech Debt Styles Setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module';
-import Logo from '~/assets/svg/logo.svg';
-import IconArrowDown from '~/assets/svg/icon-arrow-down.svg';
+import Logo_SA from '~/assets/logos/Logo_SA.svg';
+import Icon_arrow_down from '~/assets/icons/Icon_arrow_down.svg';
 
 const cn = classNames.bind(styles);
 
@@ -10,14 +10,14 @@ export default function Header() {
   return (
     <header className={cn('header')}>
       <a className={cn('brand-wrapper')} href="#">
-        <Logo className={cn('image')} alt="Sourcery Academy logo" />
+        <Logo_SA className={cn('image')} alt="Sourcery Academy logo" />
         <h1 className={cn('brand-name')}>Sourcery Academy</h1>
       </a>
       <nav className={cn('nav')}>
         <a href="#">About us</a>
         <a href="#">
           Academies
-          <IconArrowDown className={cn('icon')} alt="Arrow down icon" />
+          <Icon_arrow_down className={cn('icon')} alt="Arrow down icon" />
         </a>
         <a href="#">Media</a>
         <a href="#">Register</a>
