78cb25d89d9b6f391f397e486fd0914b62ccf25a
violent-code <violent-intent@protonmail.com>
2022-11-24 21:18:31 +0200

    SR22S-17: render PageLayout, pass classname prop

--- src/routes/routing.jsx
+++ src/routes/routing.jsx
@@ -1,19 +1,22 @@
 import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from './routeConfig';
+import PageLayout from '../components/PageLayout';
 
 export const RouteManager = React.memo(() => {
   return (
     <Routes>
       {Object.entries(routeConfig).map(([key, route]) => {
-        const { component: Component, path } = route;
+        const { component: Component, path, layout } = route;
         return (
           <Route
             key={key}
             path={path}
             element={
               <Suspense fallback={<>Loading...</>}>
-                <Component />
+                <PageLayout layoutName={layout}>
+                  <Component />
+                </PageLayout>
               </Suspense>
             }
           ></Route>
