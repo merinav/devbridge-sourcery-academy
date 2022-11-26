92d33247775c9522fcdda66f17afe158cbe4ad6e
merinav <81377750+merinav@users.noreply.github.com>
2022-11-26 17:20:42 +0200

    SR22S-20 update paddings, set body styles for big screens, clean up unnecesarry styling

--- src/pages/Home/PromoSection/PromoSection.jsx
+++ src/pages/Home/PromoSection/PromoSection.jsx
@@ -7,29 +7,25 @@ const cn = classNames.bind(styles);
 
 const PromoSection = () => {
   return (
-    <section className={cn('container')}>
-      <section className={cn('content-text')}>
-        <h1 className={cn('content-text__heading')}>
+    <section className={cn('promo-section')}>
+      <section className={cn('section-text')}>
+        <h1 className={cn('section-text__heading')}>
           Sourcery <br></br>
           Academy
         </h1>
-        <p className={cn('content-text__text')}>
+        <p className={cn('section-text__text')}>
           Sourcery academy is our free of charge education program created to
           allow students to improve their skills and prepare for a career in the
           IT industry. There are three academies available. They serve as our
           long- term commitment to increase the number of students in Lithuania
           entering information technologies studies.
         </p>
-        <p className={cn('content-text__button')}>BUTTON WILL BE HERE</p>
+        <p className={cn('section-text__button')}>BUTTON WILL BE HERE</p>
       </section>
-
-      <section className={cn('content-image')}>
-        <figure className={cn('content-image__image')}>
-          <Image_Promo />
-        </figure>
-      </section>
+      <figure className={cn('section-image')}>
+        <Image_Promo />
+      </figure>
     </section>
   );
 };
-
 export default PromoSection;
