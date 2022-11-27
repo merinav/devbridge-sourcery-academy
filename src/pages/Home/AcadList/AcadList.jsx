7f302df900c93755587ed2c10711ed077bd91a48
violent-code <violent-intent@protonmail.com>
2022-11-27 18:40:32 +0200

    SR22S-21: init branch, create components

---
+++ src/pages/Home/AcadList/AcadList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './AcadList.module';
+
+function AcadList(props) {
+  return <div>AcadList</div>;
+}
+
+export default AcadList;
