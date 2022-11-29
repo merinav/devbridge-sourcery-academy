6e3ddcf8e1880f0e8e11abd178bc04fbe78a67e0
violent-code <violent-intent@protonmail.com>
2022-11-30 01:46:12 +0200

    SR22S-21: add dynamic svg rendering with state

--- src/pages/Home/AcadList/AcadList.jsx
+++ src/pages/Home/AcadList/AcadList.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import classNames from 'classnames/bind';
 
 import AcadSection from './AcadSection';
+
 import Img_Developers from '/src/assets/images/Image_Academy_Developers.svg';
 import Img_Testers from '/src/assets/images/Image_Academy_Testers.svg';
 import Img_FrontEnd from '/src/assets/images/Image_Academy_Front_End.svg';
 import Img_Kids from '/src/assets/images/Image_Academy_Kids.svg';
 
-import Path_Developers from '/src/assets/images/Path_Academy_Developers.svg';
-import Path_Testers from '/src/assets/images/Path_Academy_Testers.svg';
-import Path_Kids from '/src/assets/images/Path_Academy_Kids.svg';
+import Path_Developers from '/src/assets/images/Path_Academy_Developers_2.svg';
+import Path_Testers from '/src/assets/images/Path_Academy_Testers_2.svg';
+import Path_Kids from '/src/assets/images/Path_Academy_Kids_2.svg';
 
 import styles from './AcadList.module';
 
@@ -22,6 +23,7 @@ const academyData = {
     paragraph:
       'A free of charge three-month cutting-edge technology course, available in the Spring and Fall of each year. Students are invited to the Devbridge in Kaunas and Vilnius offices to attend lectures and create custom projects with the help of software development professionals.',
     img: Img_Developers,
+    svgPath: Path_Developers,
     invert: false,
   },
   testers: {
@@ -30,6 +32,7 @@ const academyData = {
     paragraph:
       'Top-notch studies for the future Test Engineers. Students join the Devbridge team as a paid employee for the Academy period (two months) to learn and work. Ones who finish the Academy successfully are invited to continue employment on full time.',
     img: Img_Testers,
+    svgPath: Path_Testers,
     invert: true,
   },
   frontEnd: {
@@ -38,6 +41,7 @@ const academyData = {
     paragraph:
       'Start your career in software development with the intense program at Sourcery for Front-End Developers. Complete the admission and enter our free of charge two-month academy this autumn in Vilnius to learn from our top specialists.',
     img: Img_FrontEnd,
+    svgPath: Img_FrontEnd, //hacky fix, but it works
     invert: true,
   },
   kids: {
@@ -46,6 +50,7 @@ const academyData = {
     paragraph:
       'Sourcery for Kids seeks to educate kids giving them opportunity to learn and apply technological product development skills. For the entire academic year, the children will study key aspects of programming, from logical concepts and functions, through to variables, sequences, coordinates, and movement.',
     img: Img_Kids,
+    svgPath: Path_Kids,
     invert: false,
   },
 };
@@ -53,18 +58,10 @@ const academyData = {
 function AcadList() {
   return (
     <div className={cn('academy-list')}>
-      <AcadSection
-        data={academyData.developers}
-        img={Img_Developers}
-        bgSvg={Path_Developers}
-      />
-      <AcadSection
-        data={academyData.testers}
-        img={Img_Testers}
-        bgSvg={Path_Testers}
-      />
-      <AcadSection data={academyData.frontEnd} img={Img_FrontEnd} />
-      <AcadSection data={academyData.kids} img={Img_Kids} bgSvg={Path_Kids} />
+      <AcadSection data={academyData.developers} />
+      <AcadSection data={academyData.testers} />
+      <AcadSection data={academyData.frontEnd} />
+      <AcadSection data={academyData.kids} />
     </div>
   );
 }
--- src/pages/Home/AcadList/AcadSection/AcadSection.jsx
+++ src/pages/Home/AcadList/AcadSection/AcadSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
@@ -7,31 +7,46 @@ import styles from './AcadSection.module';
 
 const cn = classNames.bind(styles);
 
-function AcadSection({ data, img: Img, bgSvg: BgSvg }) {
-  const invert = data.invert ? 'academy-section--inv' : '';
+function AcadSection({ data }) {
+  const {
+    id,
+    title,
+    paragraph,
+    img: Img,
+    svgPath: SvgPath,
+    invert: invertBtn,
+  } = data;
+
+  const [showPath, setShowPath] = useState(false);
+
+  useEffect(() => {
+    const windowResizeHandler = () =>
+      window.innerWidth > 880 ? setShowPath(true) : setShowPath(false);
+
+    window.addEventListener('resize', windowResizeHandler);
+    return () => window.removeEventListener('resize', windowResizeHandler);
+  }, []);
+
+  const invert = invertBtn ? 'academy-section--inv' : '';
+  const imgModifier = showPath ? `img__${id}--path` : '';
+
+  const renderedImage = showPath ? <SvgPath /> : <Img />;
 
   return (
     <section
-      className={cn([
-        'academy-section',
-        `academy-section--${data.id}`,
-        `${invert}`,
-      ])}
+      className={cn(['academy-section', `academy-section--${id}`, `${invert}`])}
     >
       <div className={cn('academy-section__description')}>
         <AcadDescription
-          title={data.title}
-          paragr={data.paragraph}
-          invertBtn={data.invert}
+          title={title}
+          paragr={paragraph}
+          invertBtn={!!invert}
         />
       </div>
 
       <div className={cn('academy-section__img')}>
-        <figure className={cn(`img__${data.id}`)}>
-          <Img />
-          {BgSvg && (
-            <div className={cn(['path', `path--${data.id}`])}>{<BgSvg />}</div>
-          )}
+        <figure className={cn(`img__${id}`, `${imgModifier}`)}>
+          {renderedImage}
         </figure>
       </div>
     </section>
@@ -40,8 +55,6 @@ function AcadSection({ data, img: Img, bgSvg: BgSvg }) {
 
 AcadSection.propTypes = {
   data: PropTypes.object.isRequired,
-  img: PropTypes.func.isRequired,
-  bgSvg: PropTypes.func,
 };
 
 export default AcadSection;
