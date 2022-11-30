1faed7f318cff2c04fa0dbfaf18f7d6947240f4e
merinav <81377750+merinav@users.noreply.github.com>
2022-11-30 18:18:11 +0200

    SR22S-22 update styling, svg icons, remove hamburger from home.jsx

--- src/components/Header/Hamburger/HamburgerButton.jsx
+++ src/components/Header/Hamburger/HamburgerButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import Icon_hamburger_closed from '../../../assets/icons/Icon_hamburger_closed.svg';
 import Icon_hamburger_open from '../../../assets/icons/Icon_hamburger_open.svg';
@@ -7,16 +7,16 @@ import styles from './HamburgerButton.module';
 const cn = classNames.bind(styles);
 
 const HamburgerButton = () => {
-  const [isOpen, setisOpen] = React.useState(false);
+  const [isOpen, setisOpen] = useState(false);
   return (
     <>
       {isOpen ? (
-        <button onClick={() => setisOpen(false)}>
-          <Icon_hamburger_closed className={cn('icon')} />
+        <button onClick={() => setisOpen(false)} className={cn('hamburger')}>
+          <Icon_hamburger_closed />
         </button>
       ) : (
-        <button onClick={() => setisOpen(true)}>
-          <Icon_hamburger_open className={cn('icon')} />
+        <button onClick={() => setisOpen(true)} className={cn('hamburger')}>
+          <Icon_hamburger_open />
         </button>
       )}
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
-import HamburgerButton from '../../components/Header/Hamburger/HamburgerButton';
 
 const Home = () => {
   return (
     <>
       <PromoSection />
       <AcademiesSection />
-
-      <HamburgerButton />
     </>
   );
 };
