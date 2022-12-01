004f1a226832a3d7b454d0a9c69e0ec46d3dcc8e
aistegerd <gerdzeviciute.yy@gmail.com>
2022-12-01 22:08:24 +0200

    SR22S-44 Remove unused imports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
-import Card from '../../components/Card';
 
 const Home = () => {
   return (
