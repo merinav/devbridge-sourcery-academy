40fd4e814b0a3dddfba560473d189c8c0acf1dae
aistegerd <gerdzeviciute.yy@gmail.com>
2022-12-06 21:34:12 +0200

    SR22S-45 create initial Testimonial Card with mock data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import useTheme from '../../hooks/useTheme';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
+import TestimonialCard from '~/pages/Home/TestimonialCard';
+import TestPhoto from '~/assets/images/Image.svg';
 
 const Home = () => {
   useTheme();
@@ -10,6 +12,12 @@ const Home = () => {
     <>
       <PromoSection />
       <AcademiesSection />
+      <TestimonialCard
+        photo={TestPhoto}
+        message="During the 3 month academy we managed to learn not only the basics, but also to create advanced web page. I am glad that I chose to apply and challenge myself. I would definitely recommend to apply for anyone who’s interested in front-end. "
+        name={'Laura Hales'}
+        academy={'Sourcery for Front-End Graduate'}
+      />
     </>
   );
 };
---
+++ src/pages/Home/TestimonialCard/TestimonialCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './TestimonialCard.module.scss';
+import PropTypes from 'prop-types';
+import Card from '~/components/Card';
+import QuotationBefore from '~/assets/icons/Icon_quotation_before.svg';
+import QuotationAfter from '~/assets/icons/Icon_quotation_after.svg';
+
+const cn = classNames.bind(styles);
+
+const TestimonialCard = ({ photo, message, name, academy }) => {
+  return (
+    <Card>
+      <div className={cn('testimonial')}>
+        <figure className={cn('testimonial__photo')}>{photo}</figure>
+        <figure className={cn('testimonial__quotation-before')}>
+          <QuotationBefore alt="Quotation mark" />
+        </figure>
+        <p className={cn('testimonial__message')}>{message}</p>
+        <figure className={cn('testimonial__quotation-after')}>
+          <QuotationAfter alt="Quotation mark" />
+        </figure>
+        <h2 className={cn('testimonial__name')}>{name}</h2>
+        <h2 className={cn('testimonial__academy')}>{academy}</h2>
+      </div>
+    </Card>
+  );
+};
+
+TestimonialCard.propTypes = {
+  // TODO: change photo propType
+  photo: PropTypes.any,
+  message: PropTypes.string,
+  name: PropTypes.string,
+  academy: PropTypes.string,
+};
+
+export default TestimonialCard;
