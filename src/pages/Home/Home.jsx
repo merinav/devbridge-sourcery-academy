d8aff1e54d3558d350ab69caa0f1d97cbd33e315
Albertas <albertas.rinkevicius@gmail.com>
2022-12-06 11:16:37 +0200

    fix/SR22S-48_theming-in-Card - implement Theming in Card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,26 @@ import React, { useEffect } from 'react';
 import useTheme from '../../hooks/useTheme';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
+import Card from '../../components/Card/Card';
 
 const Home = () => {
   useTheme();
 
   return (
     <>
+      <Card>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero illum
+        illo accusamus pariatur, ratione ipsum voluptas repellat repellendus et.
+        Incidunt fuga voluptatum at cumque, dolorum iste, voluptates
+        reprehenderit praesentium nulla corporis id qui odit temporibus sequi
+        delectus eveniet! Molestiae voluptatibus dolores sit sapiente ullam
+        suscipit voluptatum, quia minima beatae dicta nesciunt quaerat error
+        doloribus veritatis itaque, expedita tenetur est vel eaque repudiandae
+        rerum. Aut, cupiditate unde tenetur odio repellat deserunt impedit vero
+        eaque eligendi minus nostrum distinctio assumenda, eius fugit cumque vel
+        autem perferendis laborum? Odit nam illum mollitia dignissimos eaque.
+        Eos quod odit velit ducimus cupiditate accusantium consequatur culpa!
+      </Card>
       <PromoSection />
       <AcademiesSection />
     </>
--- src/pages/academy/FrontEnd/FrontEnd.jsx
+++ src/pages/academy/FrontEnd/FrontEnd.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import useTheme from '/src/hooks/useTheme';
+import Card from '../../../components/Card/Card';
 
 function FrontEnd() {
   useTheme('red');
 
-  return <h1 className="only-for-testing">/academy/front-end</h1>;
+  return (
+    <>
+      <Card>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero illum
+        illo accusamus pariatur, ratione ipsum voluptas repellat repellendus et.
+        Incidunt fuga voluptatum at cumque, dolorum iste, voluptates
+        reprehenderit praesentium nulla corporis id qui odit temporibus sequi
+        delectus eveniet! Molestiae voluptatibus dolores sit sapiente ullam
+        suscipit voluptatum, quia minima beatae dicta nesciunt quaerat error
+        doloribus veritatis itaque, expedita tenetur est vel eaque repudiandae
+        rerum. Aut, cupiditate unde tenetur odio repellat deserunt impedit vero
+        eaque eligendi minus nostrum distinctio assumenda, eius fugit cumque vel
+        autem perferendis laborum? Odit nam illum mollitia dignissimos eaque.
+        Eos quod odit velit ducimus cupiditate accusantium consequatur culpa!
+      </Card>
+      <h1 className="only-for-testing">/academy/front-end</h1>
+    </>
+  );
 }
 
 export default FrontEnd;
