fd0cdc49c679190476b735c1eb65a24a69adea2b
aistegerd <gerdzeviciute.yy@gmail.com>
2022-12-19 17:04:50 +0200

    SR22S-6 optimize Testimonial Cards with loop

--- src/components/TestimonialsSection/TestimonialsSection.jsx
+++ src/components/TestimonialsSection/TestimonialsSection.jsx
@@ -13,12 +13,14 @@ const TestimonialsSection = () => {
   useTheme();
   const testimonialUrl = useTestimonialData();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTestimonial, setSelectedTestimonial] = useState(null);
 
   useEffect(() => {
     document.body.style.overflow = isModalOpen ? 'hidden' : 'unset';
   }, [isModalOpen]);
 
-  const handleOpenModal = () => {
+  const handleOpenModal = (testimonial) => {
+    setSelectedTestimonial(testimonial);
     setIsModalOpen(true);
   };
 
@@ -30,77 +32,36 @@ const TestimonialsSection = () => {
     <section className={cn('testimonials-section')}>
       <h1 className={cn('testimonials-section__title')}>Testimonials</h1>
       <div className={cn('testimonials-section__cards')}>
-        {testimonialUrl.length > 0 && (
+        {testimonialUrl.map((testimonial) => (
           <>
             <TestimonialCard
-              photo={testimonialUrl[0].photo}
-              // TODO: This is to only simulate what happens when message too long. Remove after testing
-              message={testimonialUrl[0].message.repeat(4)}
-              name={testimonialUrl[0].name}
-              academy={testimonialUrl[0].academy}
-              openModal={handleOpenModal}
+              key={testimonial.id}
+              photo={testimonial.photo}
+              message={testimonial.message}
+              name={testimonial.name}
+              academy={testimonial.academy}
+              openModal={() => handleOpenModal(testimonial)}
             />
-            {isModalOpen && (
+            {isModalOpen && selectedTestimonial && (
               <TestimonialModal
-                photo={testimonialUrl[0].photo}
-                // TODO: This is to only simulate what happens when message too long. Remove after testing
-                message={testimonialUrl[0].message.repeat(4)}
-                name={testimonialUrl[0].name}
-                academy={testimonialUrl[0].academy}
+                photo={selectedTestimonial.photo}
+                message={selectedTestimonial.message}
+                name={selectedTestimonial.name}
+                academy={selectedTestimonial.academy}
                 closeModal={handleCloseModal}
               />
             )}
           </>
-        )}
-
-        {testimonialUrl.length > 0 && (
-          <>
-            <TestimonialCard
-              photo={testimonialUrl[1].photo}
-              // TODO: This is to only simulate what happens when message too long. Remove after testing
-              message={testimonialUrl[1].message.repeat(4)}
-              name={testimonialUrl[1].name}
-              academy={testimonialUrl[1].academy}
-              openModal={handleOpenModal}
-            />
-            {isModalOpen && (
-              <TestimonialModal
-                photo={testimonialUrl[1].photo}
-                // TODO: This is to only simulate what happens when message too long. Remove after testing
-                message={testimonialUrl[1].message.repeat(4)}
-                name={testimonialUrl[1].name}
-                academy={testimonialUrl[1].academy}
-                closeModal={handleCloseModal}
-              />
-            )}
-          </>
-        )}
-
-        {testimonialUrl.length > 0 && (
-          <>
-            <TestimonialCard
-              photo={testimonialUrl[2].photo}
-              // TODO: This is to only simulate what happens when message too long. Remove after testing
-              message={testimonialUrl[2].message.repeat(4)}
-              name={testimonialUrl[2].name}
-              academy={testimonialUrl[2].academy}
-              openModal={handleOpenModal}
-            />
-            {isModalOpen && (
-              <TestimonialModal
-                photo={testimonialUrl[2].photo}
-                // TODO: This is to only simulate what happens when message too long. Remove after testing
-                message={testimonialUrl[2].message.repeat(4)}
-                name={testimonialUrl[2].name}
-                academy={testimonialUrl[2].academy}
-                closeModal={handleCloseModal}
-              />
-            )}
-          </>
-        )}
+        ))}
       </div>
     </section>
   );
 };
 
 export default TestimonialsSection;
+
+// if (testimonial.message === testimonial.message.includes("Front-End")) {
+//   show all cards that are from Front-End;
+// } else {
+//   hide whole section;
+// }
