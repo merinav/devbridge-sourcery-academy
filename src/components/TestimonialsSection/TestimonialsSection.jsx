754227563d5690cdad59abbcf8b7e60dd5d9f70e
aistegerd <gerdzeviciute.yy@gmail.com>
2022-12-21 21:42:04 +0200

    SR22S-6 improve useFetch hook, move data fetching to Home

--- src/components/TestimonialsSection/TestimonialsSection.jsx
+++ src/components/TestimonialsSection/TestimonialsSection.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from './TestimonialsSection.module.scss';
 import TestimonialCard from '~/pages/Home/TestimonialCard';
 import TestimonialModal from '~/pages/Home/TestimonialCard/TestimonialModal';
 import useTheme from '~/hooks/useTheme';
-import { useTestimonialData } from '~/hooks/useTestimonialData';
+import PropTypes from 'prop-types';
+import styles from './TestimonialsSection.module.scss';
 
 const cn = classNames.bind(styles);
 
-const TestimonialsSection = () => {
+const TestimonialsSection = ({ testimonials, academy }) => {
   useTheme();
-  const testimonialUrl = useTestimonialData();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTestimonial, setSelectedTestimonial] = useState(null);
 
@@ -32,36 +30,39 @@ const TestimonialsSection = () => {
     <section className={cn('testimonials-section')}>
       <h1 className={cn('testimonials-section__title')}>Testimonials</h1>
       <div className={cn('testimonials-section__cards')}>
-        {testimonialUrl.map((testimonial) => (
-          <>
-            <TestimonialCard
-              key={testimonial.id}
-              photo={testimonial.photo}
-              message={testimonial.message}
-              name={testimonial.name}
-              academy={testimonial.academy}
-              openModal={() => handleOpenModal(testimonial)}
-            />
-            {isModalOpen && selectedTestimonial && (
-              <TestimonialModal
-                photo={selectedTestimonial.photo}
-                message={selectedTestimonial.message}
-                name={selectedTestimonial.name}
-                academy={selectedTestimonial.academy}
-                closeModal={handleCloseModal}
-              />
+        <>
+          {testimonials.length > 0 &&
+            testimonials.map(
+              (testimonial) =>
+                testimonial.academy === academy && (
+                  <TestimonialCard
+                    key={testimonial.id}
+                    photo={testimonial.photo}
+                    message={testimonial.message}
+                    name={testimonial.name}
+                    academy={testimonial.academy}
+                    openModal={() => handleOpenModal(testimonial)}
+                  />
+                )
             )}
-          </>
-        ))}
+        </>
+        {isModalOpen && selectedTestimonial && (
+          <TestimonialModal
+            photo={selectedTestimonial.photo}
+            message={selectedTestimonial.message}
+            name={selectedTestimonial.name}
+            academy={selectedTestimonial.academy}
+            closeModal={handleCloseModal}
+          />
+        )}
       </div>
     </section>
   );
 };
 
-export default TestimonialsSection;
+TestimonialsSection.propTypes = {
+  testimonials: PropTypes.array,
+  academy: PropTypes.string.isRequired,
+};
 
-// if (testimonial.message === testimonial.message.includes("Front-End")) {
-//   show all cards that are from Front-End;
-// } else {
-//   hide whole section;
-// }
+export default TestimonialsSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,33 +2,21 @@ import React, { useEffect, useState } from 'react';
 import useTheme from '../../hooks/useTheme';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
-import TestimonialCard from '~/pages/Home/TestimonialCard';
-import { useTestimonialData } from '~/hooks/useTestimonialData';
-import TestimonialModal from '~/pages/Home/TestimonialCard/TestimonialModal';
+import useFetch from '~/hooks/useFetch';
 import TestimonialsSection from '~/components/TestimonialsSection';
 
 const Home = () => {
   useTheme();
-  const testimonialUrl = useTestimonialData();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    document.body.style.overflow = isModalOpen ? 'hidden' : 'unset';
-  }, [isModalOpen]);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  const testimonials = useFetch('https://www.jurele.lt/testimonials.json');
 
   return (
     <>
       <PromoSection />
       <AcademiesSection />
-      <TestimonialsSection />
+      <TestimonialsSection
+        testimonials={testimonials}
+        academy={'Sourcery for Front-End Graduate'}
+      />
     </>
   );
 };
