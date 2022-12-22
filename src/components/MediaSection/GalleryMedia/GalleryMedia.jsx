3f61e76b5310760a0215eeb2e683fd1a0622548d
Albertas <albertas.rinkevicius@gmail.com>
2022-12-22 21:32:29 +0200

    feat/SR22S-49 - corrections based on PR comments

--- src/components/MediaSection/GalleryMedia/GalleryMedia.jsx
+++ src/components/MediaSection/GalleryMedia/GalleryMedia.jsx
@@ -7,99 +7,70 @@ import styles from './GalleryMedia.module';
 const cn = classNames.bind(styles);
 
 const GalleryMedia = ({ data }) => {
-  let MAX_MEDIA_ITEMS;
-
-  switch (true) {
-    case data.length < 1:
-      MAX_MEDIA_ITEMS = 0;
-      break;
-
-    case data.length < 2:
-      MAX_MEDIA_ITEMS = 1;
-      break;
-
-    case data.length < 3:
-      MAX_MEDIA_ITEMS = 2;
-      break;
-
-    case data.length < 4:
-      MAX_MEDIA_ITEMS = 3;
-      break;
-
-    case data.length < 5:
-      MAX_MEDIA_ITEMS = 4;
-      break;
-
-    case data.length < 6:
-      MAX_MEDIA_ITEMS = 5;
-      break;
-
-    default:
-      MAX_MEDIA_ITEMS = 6;
-      break;
-  }
+  let maxMediaItems;
+  maxMediaItems = Math.min(data.length, 6);
 
   return (
     <>
-      {MAX_MEDIA_ITEMS > 0 && (
+      {maxMediaItems > 0 && (
         <div
           className={cn(
             'gallery',
-            `${MAX_MEDIA_ITEMS === 1 ? 'gallery--1-item' : ''}`,
-            `${MAX_MEDIA_ITEMS === 2 ? 'gallery--2-items' : ''}`,
-            `${MAX_MEDIA_ITEMS === 3 ? 'gallery--3-items' : ''}`,
-            `${MAX_MEDIA_ITEMS === 4 ? 'gallery--4-items' : ''}`,
-            `${MAX_MEDIA_ITEMS === 5 ? 'gallery--5-items' : ''}`
+            `${
+              maxMediaItems === 1
+                ? 'gallery--' + maxMediaItems + '-item'
+                : maxMediaItems !== 6
+                ? 'gallery--' + maxMediaItems + '-items'
+                : ''
+            }`
           )}
         >
-          {data
-            .filter((item, index) => index < MAX_MEDIA_ITEMS)
-            .map((item, index) =>
-              item.type === 'image' ? (
-                <div
-                  className={cn(
-                    'media-container',
-                    `media-container-${index + 1}`
-                  )}
-                  key={index}
+          {data.slice(0, maxMediaItems).map((item, index) =>
+            item.type === 'image' ? (
+              <figure
+                className={cn(
+                  'media-container',
+                  `media-container-${index + 1}`
+                )}
+                key={index}
+              >
+                <img
+                  className={cn('media-item', `image-${index + 1}`)}
+                  src={item.thumbnail}
+                  alt="Sourcery academy gallery image"
+                />
+              </figure>
+            ) : (
+              <figure
+                className={cn(
+                  'media-container',
+                  `media-container-${index + 1}`
+                )}
+                key={index}
+              >
+                <video
+                  className={cn('media-item', `video-${index + 1}`)}
+                  width="auto"
+                  height="218"
+                  poster={item.thumbnail}
                 >
-                  <img
-                    className={cn('media-item', `image-${index + 1}`)}
-                    src={item.thumbnail}
-                    alt="Sourcery academy gallery image"
+                  <source
+                    src={item.src}
+                    type={'video/' + item.src.split('.').pop()}
                   />
+                  <p>
+                    Your browser doesn&quot;t support HTML video. Here is a{' '}
+                    <a href={item.src}>link to the video</a> instead.
+                  </p>
+                </video>
+                <div className={cn('video-controls')}>
+                  <button type="button" className={cn('play-button')}>
+                    <Icon_play />
+                  </button>
                 </div>
-              ) : (
-                <div
-                  className={cn(
-                    'media-container',
-                    `media-container-${index + 1}`
-                  )}
-                  key={index}
-                >
-                  <video
-                    className={cn('media-item', `video-${index + 1}`)}
-                    width="auto"
-                    height="218"
-                    poster={item.thumbnail}
-                  >
-                    <source
-                      src={item.src}
-                      type={'video/' + item.src.split('.').pop()}
-                    />
-                    <p>
-                      Your browser doesn&quot;t support HTML video. Here is a{' '}
-                      <a href={item.src}>link to the video</a> instead.
-                    </p>
-                  </video>
-                  <div className={cn('video-controls')}>
-                    <button type="button" className={cn('play-button')}>
-                      <Icon_play />
-                    </button>
-                  </div>
-                </div>
-              )
-            )}
+              </figure>
+            )
+          )}
         </div>
       )}
     </>
