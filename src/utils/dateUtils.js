798736ebbaf68a1e65a31334ddf0b2dd86ba9b10
merinav <81377750+merinav@users.noreply.github.com>
2022-12-22 10:51:23 +0200

    SR22S-60 remove console.log

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -5,14 +5,12 @@ const returnDate = (date1, date2) => {
       month: date1.toLocaleString('en-GB', { month: 'short' }),
     },
   };
-  console.log(result);
   if (date2 !== undefined) {
     result.endDate = {
       day: date2.getDate(),
       month: date2.toLocaleString('en-GB', { month: 'short' }),
     };
   }
-  console.log(result);
   return result;
 };
 
