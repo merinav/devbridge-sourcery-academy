19e45c79b8d177447aa0701279af6b20f438c7e1
merinav <81377750+merinav@users.noreply.github.com>
2022-12-30 13:30:33 +0200

    SR22S-57 commit fix code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useTheme from '../../hooks/useTheme';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
-import { useTestimonialData } from '~/hooks/useTestimonialData';
-import TestimonialModal from '../../components/TestimonialCard/TestimonialModal/TestimonialModal';
-import TestimonialCard from '../../components/TestimonialCard';
 import MediaSection from '/src/components/MediaSection';
 
 const Home = () => {
   useTheme();
-  const testimonialUrl = useTestimonialData();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    document.body.style.overflow = isModalOpen ? 'hidden' : 'unset';
-  }, [isModalOpen]);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <>
