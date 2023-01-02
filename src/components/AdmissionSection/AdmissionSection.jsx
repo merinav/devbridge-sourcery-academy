9bb8402c63373e304d71f78888b9fc569adebc53
Albertas <albertas.rinkevicius@gmail.com>
2023-01-02 19:18:35 +0200

    feat/SR22S-61 - create Admission Section component first lines of code

---
+++ src/components/AdmissionSection/AdmissionSection.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+import ParticlesAdmission from '/src/assets/images/Background_particles_Admission.svg';
+import PathAdmission from '/src/assets/images/Path_Admission.svg';
+import styles from './AdmissionSection.module';
+
+const cn = classNames.bind(styles);
+
+const AdmissionSection = () => {
+  return (
+    <section className={cn('admission-section')} id="admission-section">
+      <div className={cn('admission-section__text-wrapper')}>
+        <h2 className={cn('admission-section__heading')}>The Admission</h2>
+        <h3>Introduction</h3>
+        <p>
+          During your first lecture we will introduce the Sourcery academy, tell
+          you more about what we do as developers, get to know your fellow team
+          members, lecturers and mentors.
+        </p>
+        <h3>You will learn:</h3>
+        <ul>
+          <li>React</li>
+          <li>JPA</li>
+          <li>Spring Boot</li>
+        </ul>
+        <h3>An interview</h3>
+        <p>
+          Only the top candidates after the final exam are invited to a job
+          interview which will challenge your technical skills and English
+          knowledge.
+        </p>
+      </div>
+      <div className={cn('admission-section__calendar-wrapper')}>
+        <h3>Dates</h3>
+
+        <div>date1</div>
+        <div>date2</div>
+        <div>date3</div>
+
+        <PathAdmission />
+
+        <ParticlesAdmission
+          className={cn('admission-section__particles')}
+          aria-hidden="true"
+        />
+      </div>
+    </section>
+  );
+};
+
+// AdmissionSection.propTypes = {
+//   prop: PropTypes.string.isRequired,
+// };
+
+export default AdmissionSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,15 @@ import useTheme from '../../hooks/useTheme';
 import PromoSection from './PromoSection';
 import AcademiesSection from './AcademiesSection';
 import MediaSection from '/src/components/MediaSection';
+import AdmissionSection from '../../components/AdmissionSection/AdmissionSection';
 
 const Home = () => {
   useTheme();
 
   return (
     <>
+      <AdmissionSection />
+
       <PromoSection />
       <AcademiesSection />
       <MediaSection />
