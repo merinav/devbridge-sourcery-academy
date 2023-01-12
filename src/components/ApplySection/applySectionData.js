29dd0611fdc36c527a2bc155f4e2dad47e3c2f7b
aistegerd <gerdzeviciute.yy@gmail.com>
2023-01-12 16:25:15 +0200

    SR22S-64 route to registration

--- src/components/ApplySection/applySectionData.js
+++ src/components/ApplySection/applySectionData.js
@@ -1,21 +1,23 @@
+import { routes } from '~/routes';
+
 const data = {
   DEVELOPERS: {
     imagePath: '/assets/images/Image_Apply_Academy_Developers.svg',
     paragraphData:
       'Entrance test results define a priority list of candidates for developers. We accept 20 candidates with highest marks into academy.',
-    buttonProps: { href: '/register' },
+    buttonProps: { to: routes.register },
   },
   FRONTEND: {
-    imagePath: '/assets/images/Image_Apply_Academy_Frontend.svg',
+    imagePath: '/assets/images/Image_Apply_Academy_Front_End.svg',
     paragraphData:
       'Start your career in software development with the intense program at Sourcery for Front-End Developers.',
-    buttonProps: { href: '/register' },
+    buttonProps: { to: routes.register },
   },
   TESTERS: {
     imagePath: '/assets/images/Image_Apply_Academy_Testers.svg',
     paragraphData:
       'Set your inner tester free and apply to empower yourself with the top-notch studies for the future Test Engineers!',
-    buttonProps: { href: '/register' },
+    buttonProps: { to: routes.register },
   },
 };
 
