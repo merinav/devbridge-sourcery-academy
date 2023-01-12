dc7da8425c53317dc5378ac5464aa9c836abc01c
Albertas <albertas.rinkevicius@gmail.com>
2023-01-12 13:29:58 +0200

    feat/SR22S-81 - create Filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
+import Button from '/src/components/Button/Button';
 import styles from './Filter.module';
 
 const cn = classNames.bind(styles);
 
-const Filter = () => {
-  return <div className={cn('filter')}>Filter component</div>;
+const Filter = ({ data }) => {
+  if (Array.isArray(data) && data.length && typeof data !== 'undefined') {
+    return (
+      <div className={cn('filter-wrapper')}>
+        {data.map((item, index) => (
+          <Button
+            key={index}
+            onClick={() => console.log('button click #' + Number(index + 1))} // TODO remove after testing
+          >
+            {item}
+          </Button>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 Filter.propTypes = {
-  // prop: PropTypes.string,
+  data: PropTypes.array,
 };
 
 export default Filter;
