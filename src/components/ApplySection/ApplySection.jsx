30da5845f4c7f0a480b6e8a371841d8ee60c88ab
aistegerd <gerdzeviciute.yy@gmail.com>
2023-01-12 16:06:17 +0200

    SR22S-64 add path and particles

--- src/components/ApplySection/ApplySection.jsx
+++ src/components/ApplySection/ApplySection.jsx
@@ -4,6 +4,8 @@ import PromoSection from '~/components/PromoSection';
 import applySectionData from '~/components/ApplySection/applySectionData';
 import PropTypes from 'prop-types';
 import { ACADEMIES } from '~/constants/constants';
+import PathApply from '~/assets/images/Path_Apply.svg';
+import ParticlesApply from '~/assets/images/Background_particles_Apply_Section.svg';
 import styles from './ApplySection.module.scss';
 
 const cn = classNames.bind(styles);
@@ -16,13 +18,20 @@ const ApplySection = ({ academy }) => {
   };
 
   return (
-    <PromoSection
-      data={{ ...dataApplicableToAllSections, ...applySectionData[academy] }}
-    >
-      <figure className={cn('apply__img')}>
-        <img src={applySectionData[academy].imagePath} alt="" />
-      </figure>
-    </PromoSection>
+    <section className={cn('apply-section')}>
+      <PromoSection
+        data={{ ...dataApplicableToAllSections, ...applySectionData[academy] }}
+      >
+        <figure className={cn('apply-section__img')}>
+          <img src={applySectionData[academy].imagePath} alt="" />
+        </figure>
+        <PathApply className={cn('apply-section__path')} aria-hidden="true" />
+        <ParticlesApply
+          className={cn('apply-section__particles')}
+          aria-hidden="true"
+        />
+      </PromoSection>
+    </section>
   );
 };
 
