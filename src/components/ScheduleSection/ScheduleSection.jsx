bcb58f0cdfe591d9355bc57a672924d4a065cdb6
Albertas <albertas.rinkevicius@gmail.com>
2023-01-14 14:09:30 +0200

    feat/SR22S-82 - create basic structure for ScheduleSection

--- src/components/ScheduleSection/ScheduleSection.jsx
+++ src/components/ScheduleSection/ScheduleSection.jsx
@@ -1,8 +1,15 @@
 import React, { useMemo } from 'react';
+import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
+import ParticlesScheduleTop from '../../assets/images/Background_particles_Schedule_top.svg';
+import ParticlesScheduleBottom from '../../assets/images/Background_particles_Schedule_bottom.svg';
+import PathSchedule from '../../assets/images/Path_Schedule.svg';
 import { ACADEMIES } from '/src/constants/constants.js';
 import { scheduleSectionsData } from './scheduleSectionsData';
 import ExpandableCard from './ExpandableCard/ExpandableCard';
+import styles from './ScheduleSection.module';
+
+const cn = classNames.bind(styles);
 
 const ScheduleSection = ({ academy }) => {
   const data = useMemo(() => {
@@ -20,10 +27,36 @@ const ScheduleSection = ({ academy }) => {
 
   return (
     <>
-      {data &&
-        data.map((lecture) => (
-          <ExpandableCard lecture={lecture} key={lecture.id} />
-        ))}
+      <section className={cn('schedule-section')}>
+        <h2 className={cn('schedule-section__heading')}>Schedule</h2>
+
+        <div className={cn('schedule-section__months-wrapper')}>
+          <h3 className={cn('schedule-section__sub-heading')}>Months</h3>
+          <h3 className={cn('schedule-section__sub-heading')}>Months</h3>
+        </div>
+
+        <div className={cn('schedule-section__lectures-wrapper')}>
+          {data &&
+            data.map((lecture) => (
+              <ExpandableCard lecture={lecture} key={lecture.id} />
+            ))}
+        </div>
+
+        <PathSchedule
+          className={cn('schedule-section__path')}
+          aria-hidden="true"
+        />
+
+        <ParticlesScheduleTop
+          className={cn('schedule-section__particles-top')}
+          aria-hidden="true"
+        />
+
+        <ParticlesScheduleBottom
+          className={cn('schedule-section__particles-bottom')}
+          aria-hidden="true"
+        />
+      </section>
     </>
   );
 };
