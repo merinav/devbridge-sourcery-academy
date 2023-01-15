93c0fd31f8eafbda40e8bdef6f715d28c2f4bc12
merinav <81377750+merinav@users.noreply.github.com>
2023-01-15 14:17:09 +0200

    SR22S-10 add initial styling, image, temporary layout-main changes

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import useTheme from '/src/hooks/useTheme';
+import Image_Application_form from '~/assets/images/Image_Application_form.svg';
 
 import styles from './Register.module';
 
@@ -14,7 +15,7 @@ const Register = () => {
       <section className={cn('register-wrapper')}>
         <h1 className={cn('heading')}>Sourcery Academy Application</h1>
         <div className={cn('content-container')}>
-          <div className={cn('register-container')}>
+          <div className={cn('content-container__register-container')}>
             <div className={cn('academy-info-container')}>
               <h3 className={cn('academy-info-container__sub-heading')}>
                 Academy information
@@ -35,7 +36,9 @@ const Register = () => {
               </div>
             </div>
           </div>
-          <figure className={cn('image')}></figure>
+          <figure className={cn('content-container__image-container')}>
+            <Image_Application_form alt="" />
+          </figure>
         </div>
       </section>
     </>
