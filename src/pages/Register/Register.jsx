1a7005e8131f65188359e5ebcf1d5b9f8b7a7f12
merinav <81377750+merinav@users.noreply.github.com>
2023-01-15 13:10:27 +0200

    SR22S-10 add html structure

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import useTheme from '/src/hooks/useTheme';
 
-function Register() {
+import styles from './Register.module';
+
+const cn = classNames.bind(styles);
+
+const Register = () => {
   useTheme('purple');
 
   return (
     <>
-      <h1 style={{ color: 'var(--primary-color)' }}>/register</h1>
+      <section className={cn('register-wrapper')}>
+        <h1 className={cn('heading')}>Sourcery Academy Application</h1>
+        <div className={cn('content-container')}>
+          <div className={cn('register-container')}>
+            <div className={cn('academy-info-container')}>
+              <h3 className={cn('academy-info-container__sub-heading')}>
+                Academy information
+              </h3>
+              <div className={cn('academy-info-container__academy-filter')}>
+                FILTER component
+              </div>
+              <div className={cn('academy-info-container__city')}>
+                FILTER component
+              </div>
+            </div>
+            <div className={cn('personal-info-container')}>
+              <h3 className={cn('personal-info-container__sub-heading')}>
+                Personal information
+              </h3>
+              <div className={cn('personal-info-container__form')}>
+                FORM component
+              </div>
+            </div>
+          </div>
+          <figure className={cn('image')}></figure>
+        </div>
+      </section>
     </>
   );
-}
+};
 
 export default Register;
