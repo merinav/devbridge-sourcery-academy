7f4c72493f12a95ff37fa27988cb0b1cacee6782
violent-code <aidasbuivydas@gmail.com>
2023-01-16 23:00:40 +0200

    SR22S-7: implement swiper slider carousel

--- src/components/MediaSection/GalleryMedia/GalleryMediaCarousel/GalleryMediaCarousel.jsx
+++ src/components/MediaSection/GalleryMedia/GalleryMediaCarousel/GalleryMediaCarousel.jsx
@@ -1,47 +1,99 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Keyboard } from 'swiper';
+import Icon_play from '/src/assets/icons/Icon_play.svg';
 import styles from './GalleryMediaCarousel.module.scss';
+import SliderNavigationButton from './SliderNavigationButton/SliderNavigationButton';
 
 const cn = classNames.bind(styles);
-const slickSliderSettings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-};
 
-const GalleryMediaCarousel = ({ mediaData }) => {
+const MEDIA_ITEMS_TO_SHOW = 30;
+
+const GalleryMediaCarousel = ({ mediaData, selectedMediaItem }) => {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  const sliderNavigationHandler = () => {};
+
   return (
-    <Slider {...slickSliderSettings}>
-      {mediaData.map((mediaItem) => (
-        <div key={mediaItem.src} className={cn('slick-slider__slide')}>
-          {mediaItem.type === 'image' ? (
-            <figure>
-              <img
-                src={mediaItem.src}
-                alt={`Photo from ${mediaItem.academy} academy`}
-              />
-            </figure>
+    <div className={cn('swiper-wrapper')}>
+      <div className={cn('swiper-navigation-wrapper')}>
+        <SliderNavigationButton
+          direction={'previous'}
+          ref={prevRef}
+          sliderNavigationHandler={sliderNavigationHandler}
+        />
+        <SliderNavigationButton
+          direction={'next'}
+          ref={nextRef}
+          sliderNavigationHandler={sliderNavigationHandler}
+        />
+      </div>
+
+      <Swiper
+        grabCursor={true}
+        centeredSlides={true}
+        modules={[Navigation, Pagination, Keyboard]}
+        autoHeight={true}
+        loop={true}
+        initialSlide={selectedMediaItem}
+        navigation={{
+          prevEl: prevRef.current ? prevRef.current : null,
+          nextEl: nextRef.current ? nextRef.current : null,
+        }}
+        keyboard={{ enabled: true }}
+        a11y={true}
+      >
+        {mediaData.slice(0, MEDIA_ITEMS_TO_SHOW + 1).map((mediaItem) => {
+          const isImage = mediaItem.type === 'image';
+
+          return isImage ? (
+            <SwiperSlide key={mediaItem.src}>
+              <figure className={cn('carousel-slide__inner-content')}>
+                <img
+                  src={mediaItem.src}
+                  alt={`Photo from ${mediaItem.academy} academy`}
+                  className={cn('inner-content__gallery-item')}
+                />
+              </figure>
+            </SwiperSlide>
           ) : (
-            <div>
-              <video
-                src={mediaItem.src}
-                title={`Video from ${mediaItem.academy} academy`}
-              />
-            </div>
-          )}
-        </div>
-      ))}
-    </Slider>
+            <SwiperSlide key={mediaItem.src}>
+              <figure className={cn('carousel-slide__inner-content')}>
+                <video
+                  className={cn('inner-content__gallery-item')}
+                  width="auto"
+                  height="218"
+                  poster={mediaItem.thumbnail}
+                  title={`Video thumbnail from ${mediaItem.academy} academy`}
+                >
+                  <source
+                    src={mediaItem.src}
+                    type={'video/' + mediaItem.src.split('.').pop()}
+                  />
+                  <p>
+                    Your browser doesn&quot;t support HTML video. Here is a{' '}
+                    <a href={mediaItem.src}>link to the video</a> instead.
+                  </p>
+                </video>
+                <div className={cn('video-controls')}>
+                  <button type="button" className={cn('play-button')}>
+                    <Icon_play className={cn('play-button__icon')} />
+                  </button>
+                </div>
+              </figure>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 };
 
 GalleryMediaCarousel.propTypes = {
   mediaData: PropTypes.array.isRequired,
+  selectedMediaItem: PropTypes.number,
 };
 export default GalleryMediaCarousel;
--- src/components/MediaSection/GalleryMedia/GalleryMediaModal/GalleryMediaModal.jsx
+++ src/components/MediaSection/GalleryMedia/GalleryMediaModal/GalleryMediaModal.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import classNames from 'classnames/bind';
+import classNames from 'classnames/bind';
 import Modal from '~/components/Modal';
 import GalleryMediaCarousel from '~/components/MediaSection/GalleryMedia/GalleryMediaCarousel';
-// import styles from './GalleryMediaModal.module.scss';
+import styles from './GalleryMediaModal.module.scss';
 
-// const cn = classNames.bind(styles);
+const cn = classNames.bind(styles);
 
-const GalleryMediaModal = ({ closeModalHandler, data }) => {
+const GalleryMediaModal = ({ closeModalHandler, data, selectedMediaItem }) => {
   return (
-    <Modal closeModal={closeModalHandler}>
-      <GalleryMediaCarousel mediaData={data}></GalleryMediaCarousel>
-    </Modal>
+    <div className={cn('gallery-media-modal')}>
+      <Modal closeModal={closeModalHandler} isGallery={true}>
+        <div className={cn('media-modal-inner-content')}>
+          <GalleryMediaCarousel
+            mediaData={data}
+            className={cn('media-modal__carousel')}
+            selectedMediaItem={selectedMediaItem}
+          />
+        </div>
+      </Modal>
+    </div>
   );
 };
 
 GalleryMediaModal.propTypes = {
   closeModalHandler: PropTypes.func.isRequired,
   data: PropTypes.array.isRequired,
+  selectedMediaItem: PropTypes.number,
 };
 export default GalleryMediaModal;
